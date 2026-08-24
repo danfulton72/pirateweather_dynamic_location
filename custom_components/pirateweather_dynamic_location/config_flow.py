0c617a7ff53ea7ce98d2b81a8422633ee67e6887
danfulton72 <42965092+danfulton72@users.noreply.github.com>
2026-08-24 13:15:37 +0100

    Fix Ruff import grouping

--- custom_components/pirateweather_dynamic_location/config_flow.py
+++ custom_components/pirateweather_dynamic_location/config_flow.py
@@ -5,7 +5,6 @@ from __future__ import annotations
 from typing import Any
 
 import voluptuous as vol
-
 from homeassistant import config_entries
 from homeassistant.core import callback
 from homeassistant.helpers import selector
